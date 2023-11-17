0ae97499094ad329b5e1d695b471286a8cb97401
Juan Rossi <juan.lucas.rossi@gmail.com>
2023-11-17 00:09:07 -0300

    feature: adiciona mongo na inicialização do servidor

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,3 +1,4 @@
-export const env = {
+export default {
+  mongoUrl: process.env.MONGO_URL || "mongodb://localhost:27017/paws-and-care",
   port: process.env.PORT || 3333,
 };
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,11 @@
+import { MongoHelper } from "../infra/db/mongo/mongo-helper";
 import { app } from "./config/app";
-import { env } from "./config/env";
+import env from "./config/env";
 
-app.listen(env.port, () =>
-  console.log("Server running at http://localhost:3333")
-);
+MongoHelper.connect(env.mongoUrl)
+  .then(async (_) => {
+    app.listen(env.port, () =>
+      console.log("Server running at http://localhost:3333")
+    );
+  })
+  .catch((e) => console.error(e));
